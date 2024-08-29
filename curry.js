00cc13ff176df8fc0732b459e55ff829dd7a8223
Catherine Li <catherineli808@gmail.com>
2024-08-29 10:43:08 -0700

    Curry

--- curry.js
+++ curry.js
@@ -1,3 +1,22 @@
 function curry(func) {
-    throw 'Not implemented!';
+    let numArgs
+    let called = 0
+    let orginalArgs = []
+    return function returnFunc(...args){
+        numArgs = func.length
+        called++
+        orginalArgs.push(...args)
+        if (called === numArgs) {
+            console.log(orginalArgs)
+            return func(...orginalArgs)
+        }
+        return returnFunc
+    }
 }
+
+
+const empty = () => 0;
+const square = (a) => a * a;
+const mul = (a, b) => a * b;
+const curried = curry(mul);
+console.log(curried())
