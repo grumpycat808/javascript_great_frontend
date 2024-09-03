3452a31843dee8b8255aae858d37f4b5c5056282
Catherine Li <catherineli808@gmail.com>
2024-09-03 11:57:32 -0700

    Array prototype concat

--- arrayMap.js
+++ arrayMap.js
@@ -28,7 +28,41 @@ Array.prototype.myFilter = function (callbackFn, thisArg) {
     return returnArr
 }
 
+Array.prototype.myReduce = function (callbackFn, initialValue) {
+    if (typeof initialValue === 'undefined' && this.length === 0)
+        throw new Error('Invalid')
+    if (!this.length) return initialValue
+    let start = 0
+    if (initialValue === undefined) {
+        initialValue = this[0]
+        start = 1
+    }
+    let accumulator = initialValue
 
-const myArr = [1, 2, 3, 4];
-console.log(myArr)
-myArr.myFilter((value) => value < 3);
+    for (let index = start; index < this.length; index++) {
+        if (this.hasOwnProperty(index)) {
+            // const boundFunc = callbackFn.bind(thisArg)
+            accumulator = callbackFn(accumulator, this[index], index, this)
+        }
+    }
+
+    return accumulator
+}
+Array.prototype.myConcat = function (...items) {
+    const returnArr = [...this]
+    for (let index = 0; index < items.length; index++) {
+        const element = items[index]
+        if (returnArr.hasOwnProperty(index) && element !== undefined) {
+            if (Array.isArray(element)) {
+                returnArr.push(...element)
+            } else {
+                returnArr.push(element)
+            }
+        }
+    }
+    console.log("returnArr", returnArr)
+    return returnArr
+}
+
+const arr = [1,undefined, 2]
+arr.myConcat(3, 4);
