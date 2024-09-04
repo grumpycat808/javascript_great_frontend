2c8e3a6ba4d6a4fee7d85e568057b9a5e41efce6
Catherine Li <catherineli808@gmail.com>
2024-09-04 08:25:25 -0700

    Completed fill.js

--- fill.js
+++ fill.js
@@ -1,13 +1,17 @@
 function fill(array, value, start = 0, end = array.length) {
     if (start < 0) start = array.length + start
     if (end < 0) end = array.length + end
-    if (start >= array.length) return array
+    // if (start >= array.length) return array
 
     for (let index = start; index < end; index++) {
-        array[index] = value
+
+        if(array.hasOwnProperty(index)) {
+
+            array[index] = value
+        }
     }
     console.log(array)
     return array
 }
 
-fill([1], '*', 2, 3)
+fill([1, 2], '*', 2, 3)
