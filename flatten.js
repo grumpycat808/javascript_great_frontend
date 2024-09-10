7431cbe8585e89c1a89eba07507faedb11bb8dba
Catherine Li <catherineli808@gmail.com>
2024-09-10 06:01:54 -0700

    Practicing recursion

--- flatten.js
+++ flatten.js
@@ -4,22 +4,25 @@ function flatten(value) {
     if(!Array.isArray(value) || value.length===0) return 
     if (value.length > 1) {
         const first = value.shift();
-        // console.log("first", first)
-        return first + flatten(value)
+        const returnArr = [first];
+        returnArr.push(flatten(value))
+        return returnArr;
     } else {
         if( value.length === 1) {
             return value[0]
         }
-        
     }
 }
-console.log("flatten", flatten([1, 2, 3]))
+console.log("flatten", flatten([1, [2, [3]]]))
 
-// if(Array.isArray(value)) {
-//     flatten(value[0])
-// }
+function factorial(num) {
+    if(num === 1) return 1;
+    return num * factorial(num - 1)
+}
 
-const value = [2, 54, 6, 7];
+function fibonacci(num) {
+  if(num <= 1) return num;
+  return fibonacci(num - 1) + fibonacci(num - 2)
+}
 
-value.shift();
-console.log(value)
+console.log(fibonacci(9));
