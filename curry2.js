0ff555151300b891897812544aa5299124b14e4e
Catherine Li <catherineli808@gmail.com>
2024-09-16 10:49:20 -0700

    Curried function with this binding

--- curry2.js
+++ curry2.js
@@ -2,17 +2,17 @@ function curry(func) {
     const minArgs = func.length
     let currentArgs = []
 
-    console.log('this line 5', this)
-    function returnFunction (...args) {
+    function returnFunction(...args) {
         console.log('this line 7', this)
-        currentArgs.push(...args)
-        
+        if (args !== undefined && args.length > 0) currentArgs.push(...args)
+
+        const boundFunc = func.bind(this)
         if (currentArgs.length === minArgs) {
             const allArgs = [...currentArgs]
             currentArgs = []
-            const boundFunc = func.bind(this);
             return boundFunc(...allArgs)
         } else {
+            const boundReturnFunc = returnFunction.bind(this)
             return returnFunction
         }
     }
@@ -23,21 +23,23 @@ function multiplyThreeNumbers(a, b, c) {
     return a * b * c
 }
 
-function add(a, b) {
-    console.log("this in add", this)
-    return this.age + a + b;
-}
+// function add(a, b) {
+//     console.log('this in add', this)
+//     return this.age + a + b
+// }
 
-const curried = curry(function ( val1, val2) {
-    console.log("this", this)
-    return this.multiplier * (val1 + val2);
-  });
-
+const curried = curry(function (val1, val2) {
+    console.log('this in curried', this)
+    console.log('val1', val1);
+    console.log("val2", val2)
+    return this.multiplier * (val1 + val2)
+})
+ const curriedOne = curry(function (val1) {
+    
+    return this.multiplier * (val1)
+})
 const Person = {
     multiplier: 5,
-    mul: curried,
+    mul: curriedOne,
 }
-
-Person.mul(2);
-console.log(Person.mul(4))
-// console.log('multiply age', Person.multipleAge(4))
+console.log(Person.mul(3))
