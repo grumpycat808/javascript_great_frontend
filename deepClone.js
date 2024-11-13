495ffa7ea376c8b4ed3c1c7cf77607c69d794054
Catherine Li <catherineli808@gmail.com>
2024-11-12 16:55:34 -0800

    Completed deep clone

--- deepClone.js
+++ deepClone.js
@@ -1,19 +1,44 @@
 function deepClone(value) {
-    let returnObj = {}
+    let returnObj = Array.isArray(value) ? [] : {};
 
     function recursiveClone(item, source) {
         if (Array.isArray(item)) {
+            const newArr = [...item];
+            newArr.forEach(element => {
+                if(Array.isArray(element)) {
+                    source.push(recursiveClone(element, []));
+                } else if(typeof element === 'object'){
+                    source.push(recursiveClone(element, {}));
+                } else {
+                    source.push(element);
+                }
+            });
         } else if (typeof item === 'object') {
-        } else {
-            return item;
-        }
+            const newItem = {...item};
+            for (const key in newItem) {
+                const element = newItem[key];
+                if(Array.isArray(element)) {
+                    source[key] = recursiveClone(element, []);
+                } else if(typeof element === 'object'){
+                    // debugger;
+                    source[key] = recursiveClone(element, {});
+                } else {
+                    source[key] = element;
+                }
+            }
+        } 
+        return source;
+    }
+
+    if(value === null || typeof value === 'undefined') return value;
+    if(Array.isArray(value) || typeof value === 'object') {
+        recursiveClone(value, returnObj);
+        return returnObj;
+
+    } else {
+        return value;
     }
 }
-const bar = {cat: "dog"};
-const one = {foo: bar};
+console.log(deepClone(null))
 
-const two = {...one};
 
-two.foo.cat = "elephant";
-
-console.log(bar.cat);
