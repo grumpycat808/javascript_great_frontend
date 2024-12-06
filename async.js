577a54be4fff6e81721cc84fe15360d89eacc8ae
Catherine Li <catherineli808@gmail.com>
2024-12-06 13:27:48 -0800

    First async try

--- async.js
+++ async.js
@@ -4,9 +4,27 @@ const getUser1 = () => {
         .then((res) => console.log(res))
 }
 
+const hey = new Promise((res) => {
+        setTimeout(() => {
+            res()
+        }, 3000)
+    })
+
 const getUser2 = async() => {
-    const json = await fetch('https://randomuser.me/api/')
+
+    try {
+
+        // const json = await fetch('https://randomuser.me/api/')
+        const json = await fetch('does not exist.com');
+        const res = await json.json();
+        return res;
+    } catch (error) {
+        // return "Error with the request!"
+        throw new Error("Problem with the request")
+    }
     
 }
 
-getUser1();
+// getUser1();
+// console.log("Hey")
+getUser2().then(res => console.log("Result in then: " + res)).catch(err => console.log("in catch block " + err))
